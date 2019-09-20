266680759f1db8f61cbbc340226ba0080fd0de32
Kalen <ktwmgoo@gmail.com>
2019-09-20 16:41:25 -0700

    Java file completed

--- Lab00.java
+++ Lab00.java
@@ -1,8 +1,38 @@
-public class Lab00
-{
-   public static void main(String[] args)
-   {
+public class Lab00 {
+   public static void main(String[] args) {
+      // declaring and initializing some variables
+      int x = 5;
+      String y = "hello";
+      double z = 9.8;
 
-   
+      // printing the variables
+      System.out.println("x: " + x + " y: " + y + " z: " + z);
+
+      // a list (make an array in java)
+      int[] nums = { 3, 6, -1, 2 };
+      for (int num : nums) {
+         System.out.println(num);
+      }
+
+      // call a function
+      int numFound = charCount(y, 'l');
+      System.out.println("Found: " + numFound);
+
+      // a counting for loop
+      for (int i = 1; i < 11; i++) {
+         System.out.print(i + " ");
+      }
+
+      System.out.println();
    }
+
+   public static int charCount(String s, char c) {
+      int count = 0;
+      for (char ch = 0; ch < s.length(); ch++) {
+         if (s.charAt(ch) == c) {
+            count += 1;
+         }
+      }
+      return count;
+   };
 }
